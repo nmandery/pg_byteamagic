3f0b82ed364be18a621c41ea0a71e42b86239953
Nico Mandery <nico@nmandery.net>
2011-06-27 21:47:13 +0200

    open libmagic only once per session -- sadly PG does not call the PG_fini function

--- byteamagic.c
+++ byteamagic.c
@@ -14,12 +14,22 @@
 PG_MODULE_MAGIC;
 #endif
 
+/*#define BM_DEBUG*/
+
+#ifdef BM_DEBUG
+#define bm_debug(A)    ereport(NOTICE, (errmsg(A)));
+#else
+#define bm_debug(A)
+#endif
+
+static struct magic_set * magic_c = NULL;
+static int m_flags = 0;
 
 /* protoypes */
 static text * magic_bytea_info(bytea*, int);
 Datum byteamagic_mime(PG_FUNCTION_ARGS);
 Datum byteamagic_text(PG_FUNCTION_ARGS);
-
+void _PG_fini(void);
 
 /* db functions */
 PG_FUNCTION_INFO_V1(byteamagic_mime);
@@ -29,28 +39,39 @@ PG_FUNCTION_INFO_V1(byteamagic_text);
 static text *
 magic_bytea_info(bytea * b, int magic_flags)
 {
-
-    struct magic_set *  magic_c;
     text *              ret_text;
     const char *        magic_result;
     char *              p_magic_err;
     const char *        magic_err;
 
 
-    magic_c = magic_open(magic_flags);
     if (magic_c == NULL) {
-        ereport(ERROR,
-            (errcode(ERRCODE_EXTERNAL_ROUTINE_EXCEPTION),
-                errmsg("could not open libmagic")));
+
+        bm_debug("open the magic library");
+
+        magic_c = magic_open(magic_flags);
+        if (magic_c == NULL) {
+            ereport(ERROR,
+                (errcode(ERRCODE_EXTERNAL_ROUTINE_EXCEPTION),
+                    errmsg("could not open libmagic")));
+        }
+
+        // try to open the default magic database
+        if (magic_load(magic_c, NULL) == -1) {
+            magic_close(magic_c);
+            magic_c = NULL;
+
+             ereport(ERROR,
+                (errcode(ERRCODE_EXTERNAL_ROUTINE_EXCEPTION),
+                    errmsg("could not load the default libmagic database")));
+        }
+        m_flags = magic_flags;
     }
 
-    // try to open the default magic database
-    if (magic_load(magic_c, NULL) == -1) {
-        magic_close(magic_c);
-
-         ereport(ERROR,
-            (errcode(ERRCODE_EXTERNAL_ROUTINE_EXCEPTION),
-                errmsg("could not load the default libmagic database")));
+    // change flags if they differ from last call
+    if (magic_flags != m_flags) {
+        magic_setflags(magic_c, magic_flags);
+        m_flags = magic_flags;
     }
 
     magic_result = magic_buffer(magic_c, VARDATA_ANY(b),
@@ -71,9 +92,23 @@ magic_bytea_info(bytea * b, int magic_flags)
 
     ret_text = cstring_to_text(magic_result);
 
-    magic_close(magic_c);
-
     return ret_text;
+}
+
+/**
+ * unload callback
+ *
+ * close the magic library if it has been opened
+ *
+ * !! POSTGRES DOES NOT CALL THIS ... see PG docs
+ */
+void
+_PG_fini(void)
+{
+    if (magic_c != NULL) {
+        magic_close(magic_c);
+        magic_c = NULL;
+    }
 }
 
 
